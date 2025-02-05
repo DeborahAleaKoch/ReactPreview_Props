307137c9f28037598da2ad19c829b9545ea960e5
DeborahAleaKoch <alea-de@web.de>
2025-02-05 15:30:19 +0100

    change props to product

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,13 +3,17 @@ import { ProductDetails, products } from "../data";
 export function ProductCard() {
 	return (
 		<div className='grid grid-cols-3'>
-			{products.map((props) => {
+			{products.map((product) => {
 				return (
-					<div key={props.name} className='shadow-md m-3 p-3'>
-						<img src={props.img} alt={props.name} className='object-cover' />
-						<h3 className='text-center'>{props.name}</h3>
-						<p className='text-center'>${props.price}</p>
-						<div className='flex justify-center my-3'>
+					<div key={product.name} className='shadow-md m-3 p-3'>
+						<img
+							src={product.img}
+							alt={product.name}
+							className='object-cover'
+						/>
+						<h3 className='text-center'>{product.name}</h3>
+						<p className='text-center'>${product.price}</p>
+						<div className='flex justify-center'>
 							<button className='border-2 border-rose-400 bg-rose-300 text-white px-3 rounded-md'>
 								BUY NOW
 							</button>
